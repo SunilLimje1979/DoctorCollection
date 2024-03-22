8a0229ca1eb26a5b7d501029d1974b850704c414
unknown <SunilLimje1979>
2024-03-22 11:42:53 +0530

    DoctorCollection/

--- DoctorCollection/urls.py
+++ DoctorCollection/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("DoctorCollection/",include('app1.urls')),
+    path('DoctorCollection/admin/', admin.site.urls),
+    path('DoctorCollection/',include('app1.urls')),
 ]
